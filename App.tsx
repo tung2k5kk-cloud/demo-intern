4443094bc11227002eafd155c9dca4d8253edffb
DO HUU TUNG <tung2k5kk@gmail.com>
2026-09-08 11:20:34 +0700

    Text Input and Touch

--- App.tsx
+++ App.tsx
@@ -1,74 +1,56 @@
 import React, { useState } from 'react';
-import { StyleSheet,View, Text, Button, ScrollView, RefreshControl } from 'react-native';
-
-
+import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity } from 'react-native';
 
 const App = () => {
-  const [refershing , setRefershing] = useState(false)
-  const [item,setItem] = useState([ {key:1,name:"iteam 1"},
-    {key:2,name:"iteam 1"},
-    {key:3,name:"iteam 2"},
-    {key:4,name:"iteam 3"},
-    {key:5,name:"iteam 4"},
-    {key:6,name:"iteam 5"}, 
-    {key:7,name:"iteam 6"},
-    {key:8,name:"iteam 7"},
-    {key:9,name:"iteam 8"},
-    {key:10,name:"iteam 9"},
-    {key:11,name:"iteam 10"}]
-  )
-  const onChangeNumber = () => {
-    setItem([{key:12,name:"iteam12"}])
+  const [name,setName] = useState("");
+  const [submit,setSubmit] = useState(false);
+  const onChangeSubmit = () => {
+    setSubmit(!submit)
   }
-  const onChangeRefreshing = () => {
-    setRefershing(true)
-    setItem([...item,{key:13,name:"iteam 27"}])
-    setRefershing(false)
-  }
-  
+
+
   return (
     <View style={styles.box}>
-      <ScrollView
-      refreshControl={
-        <RefreshControl
-        refreshing= {refershing}
-        onRefresh={onChangeRefreshing}
-        />
-      }
-      >
-
-       
-      {
-        item.map((key) => {
-          return(
-            <View style={styles.item}>
-        <Text style={styles.text}>{key.name}</Text>
-        </View>
-          )
-        }
-      )
-      }
-      <Button title='Change Number' onPress={onChangeNumber}></Button>
-      </ScrollView>
+      <Text style={styles.title}>Enter your name</Text>
+      <TextInput multiline style={styles.textinput}
+      placeholder='eg Do HUU TUNG'
+      onChangeText={(value)=>setName(value)}
+      />
+      
+      {/* <Button title='Submit' onPress={onChangeSubmit}></Button> */}
+      <TouchableOpacity style={styles.button} onPress={onChangeSubmit}>
+        <Text style={styles.title}>{submit ? "Clear" : "Submited"}</Text>
+      </TouchableOpacity>
+      {submit ?
+      <Text style={styles.title}> Your name is {name}</Text>
+      : null
+       }
+      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   box: {
-    flex:1,
-    flexDirection:'column',
-    backgroundColor:'white'
-  },
-  item:{
-    margin:10,
-    backgroundColor:'blue',
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f8fafc',
   },
-  text:{
-    fontSize:50,
-    fontStyle:'italic',
+  title: {
+    fontSize: 20,
+    color: '#0f172a',
+    marginBottom: 12,
+  },
+  textinput:{
+    fontSize:15,
+    borderWidth:1,
+    borderRadius:5,
+  },
+  button:{
+    alignItems:'center',
+    width:200,
+    backgroundColor:'green'
   }
 });
 
